5ca2ea5b695d1c90e1e1dbb09cd9ab0747044f6a
Tobias Fan <TheBourneOldTomato@users.noreply.github.com>
2021-12-14 06:58:33 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,26 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// const whitelist = [
-//   "http://localhost:3000",
-//   "http://localhost:5000",
-//   "https://scratchcollective.herokuapp.com",
-// ];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     console.log("** Origin of request " + origin);
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       console.log("Origin acceptable");
-//       callback(null, true);
-//     } else {
-//       console.log("Origin rejected");
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-
-// app.use(cors(corsOptions));
-
 app.use(cors());
 
 // Connect Database
