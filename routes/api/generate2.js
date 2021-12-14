c554ad313dd1f24bb7d4c084e3e7291e9d874698
Tobias Fan <TheBourneOldTomato@users.noreply.github.com>
2021-12-14 07:00:52 -0800

    Update generate2.js

--- routes/api/generate2.js
+++ routes/api/generate2.js
@@ -15,42 +15,5 @@ router.get("/", (req, res) => {
 
   res.status(200).send("child process exit");
 
-  // child.stderr.on("error", function (err) {
-  //   console.log("generate2 - child proccess error");
-  //   console.log(err.toString());
-  // });
-
-  // child.stdout.on("data", (data) => {
-  //   console.log(`generate2 - data:${data}`);
-  // });
-
-  // child.on("exit", (code) => {
-  //   console.log("generate2 child exit");
-  //   res.status(200).send("child process exit");
-  // });
-});
-
 module.exports = router;
 
-// const runPython = (req, res) => {
-//   // spawns child process to call stylize python script
-//   console.log("runPython running....");
-
-//   const child = spawn(proces.env.PYTHON_EXEC, [
-//     "styletransfer/styletransfer.py",
-//   ]);
-
-//   child.stderr.on("error", function (err) {
-//     console.log("generate2 - child proccess error");
-//     console.log(err.toString());
-//   });
-
-//   child.stdout.on("data", (data) => {
-//     console.log(`generate2 - data:${data}`);
-//   });
-
-//   child.on("exit", (code) => {
-//     console.log("generate2 child exit");
-//     res.status(200).send("child process exit");
-//   });
-// };
